8167a9778fd759017435584ec994751bd4001638
Pedro Pacheco <pacheckdev@gmail.com>
2021-09-16 02:21:21 -0300

    Finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
+import React, { useState, useRef, useEffect } from 'react';
+
 import './App.css';
 
 function App() {
+  const [userTask, setUserTask] = useState(null);
+  const [taskList, setTaskList] = useState([
+    {
+      id: Math.random(),
+      task: 'Finish react hook crash course',
+    },
+  ]);
+  const [hasError, setHasError] = useState(false);
+  const emptyMessage = 'Get started by adding a new task.';
+
+  const inputRef = useRef(null);
+
+  const handleChange = (e) => setUserTask(e.target.value);
+
+  const handleClick = () => {
+    if (userTask) {
+      const userDetailedTask = {
+        id: Math.random(),
+        task: userTask,
+      };
+      setTaskList([...taskList, userDetailedTask]);
+      setUserTask('');
+      setHasError(false);
+    } else {
+      setHasError(true);
+    }
+  };
+
+  const handleDelete = (id) => {
+    const newTaskList = taskList.filter((task) => task.id !== id);
+    setTaskList(newTaskList);
+  };
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, [hasError]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="container-title">
+          <h1>Task List</h1>
+        </div>
+        <ul className="task-list">
+          {taskList.length > 0 ? (
+            taskList.map((task) => (
+              <li key={task.id}>
+                <p>{task.task}</p>
+                <button onClick={() => handleDelete(task.id)}>delete</button>
+              </li>
+            ))
+          ) : (
+            <h2>{emptyMessage}</h2>
+          )}
+        </ul>
+        <div className="input-task-container">
+          <input
+            placeholder="add a task"
+            onChange={handleChange}
+            value={userTask}
+            ref={inputRef}
+            className={hasError ? 'emptyInputError emptyInputBorderError' : ''}
+          />
+          <button onClick={handleClick}>Add</button>
+        </div>
+      </div>
     </div>
   );
 }
